bfbfcf5f7d02c1e8049c73e4714f3617c010dc50
JorensM <jorensmerenjanu@gmail.com>
2024-06-01 17:24:15 +0300

    feat: 'project not found' message

--- app/protected/project/[id]/page.tsx
+++ app/protected/project/[id]/page.tsx
@@ -1,15 +1,19 @@
-import { createClient } from '@/utils/supabase/server';
+// Core
 import { redirect } from 'next/navigation';
-import { FormEvent } from 'react';
+
+// Util
+import { createClient } from '@/utils/supabase/server';
+
+// Types
+import { Project } from '@/types/Project';
 
 export default async function ProjectPage( { params }: { params: { id: string }}) {
     const projectID = params.id;
 
-    let project = {
-        title: '',
-        link: '',
-        description: ''
-    }
+    let project = null as Project | null;
+
+    let isNewProject = false;
+    
 
     const supabase = createClient();
 
@@ -17,17 +21,26 @@ export default async function ProjectPage( { params }: { params: { id: string }}
 
     let isOwnProject = true;
 
-    if(projectID != 'new') {
-        const { data } = await supabase.from('projects').select().eq('id', projectID).single();
-        if(data.user_id == user?.id) {
-            console.log('is own project')
-            isOwnProject = true;
-            project = data;
-        } else {
-            isOwnProject = false;
-        }
+    
 
+    const fetchProjectIfNotNew = async () => {
+        if(projectID != 'new') {
+            const { data } = await supabase.from('projects').select().eq('id', projectID).single();
+            if(data && data.user_id == user?.id) {
+                console.log('is own project')
+                isOwnProject = true;
+                project = data;
+            } else {
+                isOwnProject = false;
+            }
+        } else {
+            isNewProject = true;
+        }
     }
+
+    console.log('project: ', project);
+
+    await fetchProjectIfNotNew();
 
     const handleSubmit = async (formData: FormData) => {
         "use server"
@@ -45,7 +58,6 @@ export default async function ProjectPage( { params }: { params: { id: string }}
                     description: formData.get('description')
                 })
                 .select()
-            console.log(res);
             const newlyCreatedProjectID = res.data![0].id;
             redirect('/project/' + newlyCreatedProjectID);
         } else {
@@ -67,20 +79,21 @@ export default async function ProjectPage( { params }: { params: { id: string }}
     }
     return(
         <div className='w-full'>
-            { isOwnProject ? 
+            { isOwnProject && (project || isNewProject) ? 
                 <>
                     <h1 className='text-2xl mb-4'>{projectID == 'new' ? 'Create new' : 'Edit'} project</h1>
                     <form action={handleSubmit} className='flex flex-col'>
                         <label>Title</label>
-                        <input name='title' id='title' defaultValue={project.title}></input>
+                        <input name='title' id='title' defaultValue={project?.title || ''}></input>
                         <label>Link</label>
-                        <input name='link' id='link' defaultValue={project.link}></input>
+                        <input name='link' id='link' defaultValue={project?.link || ''}></input>
                         <label>Description</label>
-                        <textarea name='description' id='description' rows={8} defaultValue={project.description}></textarea>
+                        <textarea name='description' id='description' rows={8} defaultValue={project?.description || ''}></textarea>
                         <button className='bg-blue-400 hover:bg-blue-500 w-fit px-6 py-2 rounded-md mt-2 ml-auto'>Save</button>
                     </form>
                 </>
                 
+            : !project ? <h1>Could not find project</h1> 
             : <h1>You do not have access to this project</h1>}
             
         </div>
